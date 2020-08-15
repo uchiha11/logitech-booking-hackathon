ee9d094d82527a21400c46de3111e7e98d9a3814
preetisom <preeti.somappa@gmail.com>
2020-08-16 01:15:20 +0530

    Updates

---
+++ precache-manifest.186987f6c9c59f666225592e1c18eacc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6af6e9ad9ffe12c35943324d53c25bf",
+    "url": "/seat/index.html"
+  },
+  {
+    "revision": "d778a86ea9998e170da3",
+    "url": "/seat/static/css/main.71128dc3.chunk.css"
+  },
+  {
+    "revision": "2ecdca6acf3be99432e1",
+    "url": "/seat/static/js/2.9404c97e.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/seat/static/js/2.9404c97e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d778a86ea9998e170da3",
+    "url": "/seat/static/js/main.e9a73b4c.chunk.js"
+  },
+  {
+    "revision": "425a09639fe5d45fef71",
+    "url": "/seat/static/js/runtime-main.17384b6a.js"
+  },
+  {
+    "revision": "2b13d641fba6cc388f56ca0efbf39298",
+    "url": "/seat/static/media/a.2b13d641.jpg"
+  },
+  {
+    "revision": "05377515ee3487681aac34497a5836b1",
+    "url": "/seat/static/media/c.05377515.jpg"
+  },
+  {
+    "revision": "ff1287a8202564575165367ad19c0521",
+    "url": "/seat/static/media/img.ff1287a8.jpg"
+  }
+]);
